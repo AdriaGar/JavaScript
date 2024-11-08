9c312e3a0292c7abe49a36a57fdf68ce78171944
AdriG <emaildeadria@gmail.com>
2024-11-08 09:38:18 +0100

    Ejercicio 2

--- Ejercicios.js
+++ Ejercicios.js
@@ -1,15 +1,20 @@
 //1.- Mostra per consola el teu nom, cognoms i any de naixement utilitzant diferents missatges de consola.
+/*
 var nom = "Adria"
 var cognom = "Garcia Velasco"
 var anyNeixement = 2004
 console.log(nom)
 console.warn(cognom)
 console.error("Nascut a " + anyNeixement)
+*/
 
 //2.- Declara 3 variables, cadascuna amb un tipus de dades diferent i mostra el valor per consola.
 const CONSTANTE = 5
 let patata = "7"
 var variable = true
+console.log(CONSTANTE)
+console.log(patata)
+console.log(variable)
 
 //3.- Mostra una variable per consola que hagis declarat posteriorment amb var. Què apareix? I si canvies la declaració a let ?
 console.log(marron)
