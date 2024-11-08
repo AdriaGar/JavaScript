ea5ff576fd291a9acf9a343c8ee426a4b046d71c
AdriG <emaildeadria@gmail.com>
2024-11-08 09:26:55 +0100

    Primer Commit

---
+++ Ejercicios.js
@@ -0,0 +1,34 @@
+//1.- Mostra per consola el teu nom, cognoms i any de naixement utilitzant diferents missatges de consola.
+var nom = "Adria"
+var cognom = "Garcia Velasco"
+var anyNeixement = 2004
+console.log(nom)
+console.warn(cognom)
+console.error("Nascut a " + anyNeixement)
+
+//2.- Declara 3 variables, cadascuna amb un tipus de dades diferent i mostra el valor per consola.
+const CONSTANTE = 5
+let patata = "7"
+var variable = true
+
+//3.- Mostra una variable per consola que hagis declarat posteriorment amb var. Què apareix? I si canvies la declaració a let ?
+console.log(marron)
+var marron = true
+console.log(marron)
+
+//4.- Declara una variable amb un valor i mostra’l per consola. Modifica el seu valor a un altre tipus de dades. Et deixa? Perquè?
+//5.- Declara una constant amb un valor i mostra’l per consola. Modifica el seu valor a un altre tipus de dades. Et deixa? Perquè?
+
+/*Els exercicis següents realitza’ls en un fitxer .js diferent i que sigui referenciat al index.html
+6.- Donats un number i un BigInt ( 1.234 i 5.678 ), calcula les quatre operacions: suma, resta,
+    divisió i multiplicació i mostra els resultats amb un sol decimal.
+7.- Calcula la hipotenusa d’un triangle on les longituds dels catets sigui de 4.
+8.- Divideix un enter entre un String. Què retorna?
+    9.- Donat un String amb el teu nom i cognoms, extreu el teu primer cognom, i mostra’l per pantalla
+indicant el número de caràcters que té.
+10.- Donat un String amb el teu nom i cognoms, esborra el caràcter de la posició 6 i mostra el
+resultat per pantalla.
+11.- Declara una variable undefined i un null. Què passa si els sumes?
+    12.- Realitza una coerció explícita i implícita d’un booleà a String.
+13.- Realitza una coerció explícita i implícita d’un String a booleà.
+14.- Realitza una coerció explícita i implícita d’un booleà a Number.*/
