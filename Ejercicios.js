23f5959909309aacb99b6dd27080c809b6c6154f
AdriG <emaildeadria@gmail.com>
2024-11-08 09:46:13 +0100

    Ejercicio 4

--- Ejercicios.js
+++ Ejercicios.js
@@ -19,11 +19,21 @@ console.log(variable)
 */
 
 //3.- Mostra una variable per consola que hagis declarat posteriorment amb var. Què apareix? I si canvies la declaració a let ?
+/*
 console.log(marron)
 var marron = true
 console.log(marron)
+*/
 
-//4.- Declara una variable amb un valor i mostra’l per consola. Modifica el seu valor a un altre tipus de dades. Et deixa? Perquè?
+//4.- Declara una variable amb un valor i mostra’l per consola.
+// Modifica el seu valor a un altre tipus de dades. Et deixa? Perquè?
+//*
+var patata = "patata"
+console.log(patata)
+patata = 7
+console.log(patata)
+//*/
+
 //5.- Declara una constant amb un valor i mostra’l per consola. Modifica el seu valor a un altre tipus de dades. Et deixa? Perquè?
 
 /*Els exercicis següents realitza’ls en un fitxer .js diferent i que sigui referenciat al index.html
