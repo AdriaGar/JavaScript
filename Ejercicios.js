322fc675db73568f514397a64e6e955608846ee6
AdriG <emaildeadria@gmail.com>
2024-11-11 11:16:46 +0100

    Ejercicio 6

--- Ejercicios.js
+++ Ejercicios.js
@@ -40,17 +40,3 @@ const PATATA = "hola mundo"
 console.log(PATATA)
 PATATA = 7
 console.log(PATATA)
-
-/*Els exercicis següents realitza’ls en un fitxer .js diferent i que sigui referenciat al index.html
-6.- Donats un number i un BigInt ( 1.234 i 5.678 ), calcula les quatre operacions: suma, resta,
-    divisió i multiplicació i mostra els resultats amb un sol decimal.
-7.- Calcula la hipotenusa d’un triangle on les longituds dels catets sigui de 4.
-8.- Divideix un enter entre un String. Què retorna?
-    9.- Donat un String amb el teu nom i cognoms, extreu el teu primer cognom, i mostra’l per pantalla
-indicant el número de caràcters que té.
-10.- Donat un String amb el teu nom i cognoms, esborra el caràcter de la posició 6 i mostra el
-resultat per pantalla.
-11.- Declara una variable undefined i un null. Què passa si els sumes?
-    12.- Realitza una coerció explícita i implícita d’un booleà a String.
-13.- Realitza una coerció explícita i implícita d’un String a booleà.
-14.- Realitza una coerció explícita i implícita d’un booleà a Number.*/
---
+++ Ejercicios2.js
@@ -0,0 +1,24 @@
+//Els exercicis següents realitza’ls en un fitxer .js diferent i que sigui referenciat al index.html
+//6.- Donats un number i un BigInt ( 1.234 i 5.678 ), calcula les quatre operacions: suma, resta,
+//divisió i multiplicació i mostra els resultats amb un sol decimal.
+var num = 1234;
+var num2 = 5678n
+
+console.log((num + Number(num2)).toFixed(1))
+console.log((num - Number(num2)).toFixed(1))
+console.log((num * Number(num2)).toFixed(1))
+console.log((num / Number(num2)).toFixed(1))
+//7.- Calcula la hipotenusa d’un triangle on les longituds dels catets sigui de 4.
+var catet1 = 4
+var cater2 = 4
+var hipotenusa = (catet1 ^2) + (cater2 *2)
+
+//8.- Divideix un enter entre un String. Què retorna?
+//9.- Donat un String amb el teu nom i cognoms, extreu el teu primer cognom, i mostra’l per pantalla
+//indicant el número de caràcters que té.
+//10.- Donat un String amb el teu nom i cognoms, esborra el caràcter de la posició 6 i mostra el
+//resultat per pantalla.
+//11.- Declara una variable undefined i un null. Què passa si els sumes?
+//12.- Realitza una coerció explícita i implícita d’un booleà a String.
+//13.- Realitza una coerció explícita i implícita d’un String a booleà.
+//14.- Realitza una coerció explícita i implícita d’un booleà a Number.*/
